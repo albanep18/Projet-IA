deb87c670e59e18d53b653a7aa1968fe99b0f01e
PiloD653 <plducruet@gmail.com>
2025-12-16 21:31:04 +0100

    Update Strategie.java

--- Strategie.java
+++ Strategie.java
@@ -1,10 +1,40 @@
 package awale;
 
-public interface Strategie {
-	int choisirCoup(boolean joueur1, int[] plateau);
+/**
+ * Adaptateur entre l'algorithme MCTS et l'interface Strategie utilisée dans Main.
+ */
+public class MCTSStrategie implements Strategie {
 
-	// public class Strategie extends Awale{
-	// int choisirCoup(boolean joueur1, int[] plateau);
+    private final int iterations;
 
-	// }
+    public MCTSStrategie(int iterations) {
+        this.iterations = iterations;
+    }
+
+    /**
+     * Appelée par Main à chaque tour.
+     * Transforme l'état réel du jeu en AwaleState pour le MCTS.
+     */
+    @Override
+    public int choisirCoup(boolean tourJ1, int[] plateau, int scoreJ1, int scoreJ2) {
+
+        // Création de l'état racine du MCTS
+        AwaleState root = new AwaleState(
+                plateau,
+                scoreJ1,
+                scoreJ2,
+                tourJ1
+        );
+
+        // Lancement de MCTS
+        return MCTS.bestMove(root, iterations);
+    }
+
+    /**
+     * Version par défaut (non utilisée ici, mais requise par l'interface)
+     */
+    @Override
+    public int choisirCoup(boolean tourJ1, int[] plateau) {
+        return choisirCoup(tourJ1, plateau, 0, 0);
+    }
 }
