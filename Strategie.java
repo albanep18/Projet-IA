e915f715078e03f7b9c56ede81f251fc98644868
albanep18 <albane.p18@gmail.com>
2025-12-18 01:07:01 +0100

    Remove commented code from Strategie.java

--- Strategie.java
+++ Strategie.java
@@ -3,8 +3,4 @@ package awale;
 public interface Strategie {
 	int choisirCoup(boolean joueur1, int[] plateau);
 
-	// public class Strategie extends Awale{
-	// int choisirCoup(boolean joueur1, int[] plateau);
-
-	// }
 }
