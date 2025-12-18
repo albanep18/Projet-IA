f23b0d6072fe6c795f7adc37ea065488b988dcd8
albanep18 <albane.p18@gmail.com>
2025-12-18 01:06:05 +0100

    Refactor Main class for game logic and strategy
    Updated the game logic to use MCTS strategy for player 2 and added validation for moves.

--- Main.java
+++ Main.java
@@ -1,29 +1,29 @@
-package awale;
 
-public class Main extends Awale {
-	public static void main(String[] args) {
-		// Configuration des joueurs
-		Joueur j1 = new Joueur("Joueur 1", true, new RandomPlayer());
-		Joueur j2 = new Joueur("Joueur 2", false, new RandomPlayer());
-		initialiserPlateau();
-		boolean tourJ1 = true;
-		while (!finpartie(j1, j2, tourJ1)) {
-			// quijoue.estJoueur1()
-			Joueur quijoue = tourJ1 ? j1 : j2;
-			System.out.println("\nTour de " + quijoue.getNom());
-			int coup = quijoue.getStrategie().choisirCoup(tourJ1, plateau);
-			int captures = jouercoup(coup, tourJ1);
-			quijoue.ajouterScore(captures);
-			System.out.println(quijoue.getNom() + " a capturé " + captures + " graines !");
-			tourJ1 = !tourJ1;
-		}
-		System.out.printf("\n🎯 Partie terminée !\n%s : %d | %s : %d\n", j1.getNom(), j1.getScore(), j2.getNom(),
-				j2.getScore());
-		if (j1.getScore() > j2.getScore())
-			System.out.println("🏆 " + j1.getNom() + " gagne !");
-		else if (j2.getScore() > j1.getScore())
-			System.out.println("🏆 " + j2.getNom() + " gagne !");
-		else
-			System.out.println("🤝 Égalité !");
-	}
+package awale;
+public class Main {
+   public static void main(String[] args) {
+       Awale jeu = new Awale();
+       jeu.initialiserPlateau();
+       Joueur j1 = new Joueur("Joueur 1", true, new RandomPlayer());
+       Joueur j2 = new Joueur("Joueur 2", false, new MCTSStrategie());
+       boolean tourJ1 = true;
+       while (!jeu.finpartie(j1, j2, tourJ1)) {
+           Joueur joueur = tourJ1 ? j1 : j2;
+           System.out.println("\nTour de " + joueur.getNom());
+           int coup = joueur.getStrategie().choisirCoup(tourJ1, jeu.plateau);
+           if (!Awale.coupvalide(coup, tourJ1, jeu.plateau)) {
+               System.out.println("Aucun coup valide !");
+               break;
+           }
+           int captures = jeu.jouercoup(coup, tourJ1);
+           joueur.ajouterScore(captures);
+           System.out.println(joueur.getNom() +
+                   " joue le trou " + coup +
+                   " et capture " + captures + " graines");
+           tourJ1 = !tourJ1;
+       }
+       System.out.println("\n🎯 Partie terminée !");
+       System.out.println(j1.getNom() + " : " + j1.getScore());
+       System.out.println(j2.getNom() + " : " + j2.getScore());
+   }
 }
