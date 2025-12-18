990e60ef9b47951ba76286d2d798cf0ec5c67f4d
albanep18 <albane.p18@gmail.com>
2025-12-18 01:06:39 +0100

    Update coup validation to include plateau parameter

--- RandomPlayer.java
+++ RandomPlayer.java
@@ -11,14 +11,13 @@ public class RandomPlayer implements Strategie {
 		List<Integer> coupsValides = new ArrayList<>();
 		int debut = joueur1 ? 0 : 6;
 		int fin = joueur1 ? 5 : 11;
-
 		for (int i = debut; i <= fin; i++) {
-			if (Awale.coupvalide(i, joueur1))
+			if (Awale.coupvalide(i, joueur1, plateau))
 				coupsValides.add(i);
 		}
-
 		if (coupsValides.isEmpty())
 			return debut; // au cas où aucun coup n’est valide
 		return coupsValides.get(rand.nextInt(coupsValides.size()));
 	}
+
 }
