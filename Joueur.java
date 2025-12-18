5f296c18b01e0841fc599f1fdf8114d430c3003c
albanep18 <albane.p18@gmail.com>
2025-12-18 01:05:40 +0100

    Refactor Joueur class structure

--- Joueur.java
+++ Joueur.java
@@ -1,36 +1,29 @@
-package awale;
 
-public class Joueur implements Strategie{
+package awale;
+public class Joueur {
 	private String nom;
 	private boolean estJoueur1;
 	private Strategie strategie;
 	private int score;
-
 	public Joueur(String nom, boolean estJoueur1, Strategie strategie) {
 		this.nom = nom;
 		this.estJoueur1 = estJoueur1;
 		this.strategie = strategie;
 		this.score = 0;
 	}
-
 	public String getNom() {
 		return nom;
 	}
-
 	public boolean estJoueur1() {
 		return estJoueur1;
 	}
-
 	public Strategie getStrategie() {
 		return strategie;
 	}
-
 	public int getScore() {
 		return score;
 	}
-
 	public void ajouterScore(int points) {
 		score += points;
 	}
 }
-
